02a266347fa3d87da22fc7b874f5de67c43156f6
BrianMichell <ecwhodie@yahoo.com>
2019-02-08 14:24:14 -0600

    Beginning to implement a CSV output  class for all the power information.

--- EnergyLogger.java
+++ EnergyLogger.java
@@ -1,13 +1,66 @@
 package frc.robot;
 
-class EnergyLogger implements Runnable{
+import java.io.*;
+import edu.wpi.first.wpilibj.Timer;
 
-    public EnergyLogger(){
+class EnergyLogger implements Runnable {
+
+    FileWriter out;
+    double now;
+    Hardware hw;
+    boolean errorWriting;
+
+    public EnergyLogger(Hardware hw) {
+        now = Timer.getFPGATimestamp();
+        this.hw = hw;
+        errorWriting = false;
+    }
+
+    public void run() {
+        try{
+            out.append(buildHeadder());
+        } catch (Exception e){
+            errorWriting = true;
+            System.err.println(e);
+        }
+        while(!Thread.interrupted()){
+            this.now = Timer.getFPGATimestamp();
+
+        }
 
     }
 
-    public void run(){
-        
+    private void save() {
+        try{
+            out.flush();
+            out.close();
+        } catch (Exception e){
+            System.err.println(e);
+        }
+    }
+
+    private void enable() {
+
+    }
+
+    private void disable() {
+
+    }
+
+    private String getCurrents(){
+        double l1, l2, l3, r1, r2, r3;
+        l1 = this.hw.pdp.getCurrent(0);
+        l2 = this.hw.pdp.getCurrent(1);
+        l3 = this.hw.pdp.getCurrent(2);
+        r1 = this.hw.pdp.getCurrent(13);
+        r2 = this.hw.pdp.getCurrent(14);
+        r3 = this.hw.pdp.getCurrent(15);
+        String ret = String.format("%f,%f,%f,%f,%f,%f\n", l1, l2, l3, r1, r2, r3);
+        return ret;
+    }
+
+    private String buildHeadder(){
+        return "Time,Battery Voltage,Power Input,L1,L2,L3,R1,R2,R3\n";
     }
 
 }
