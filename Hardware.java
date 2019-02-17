fa280319195b992f1c1b5f5425d9229cf92ec9d6
BrianMichell <ecwhodie@yahoo.com>
2019-02-16 18:20:05 -0600

    Opening pneumatics back up, removing unused stuff.

--- Hardware.java
+++ Hardware.java
@@ -10,32 +10,19 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Talon;
 
-//import com.ctre.phoenix.motorcontrol.can.*;
-
-/*
-public interface Hardware {
-    public final SpeedControllerGroup leftDrive = new SpeedControllerGroup(new Victor(2), new Victor(3));
-    public final SpeedControllerGroup rightDrive =  new SpeedControllerGroup(new Victor(0), new Victor(1));
-    public final DifferentialDrive drive = new DifferentialDrive(leftDrive, rightDrive);
-    public final PowerDistributionPanel pdp = new PowerDistributionPanel(0);
-}
-*/
+import com.ctre.phoenix.motorcontrol.can.*;
 
 public class Hardware {
 
     // Motors
-    // public final SpeedControllerGroup leftDrive = new SpeedControllerGroup(new
-    // Victor(2), new Victor(3));
-    // public final SpeedControllerGroup rightDrive = new SpeedControllerGroup(new
-    // Victor(0), new Victor(1));
     public final Talon leftDrive, rightDrive;
     public final DifferentialDrive drive;
     public final Victor thetaOne, thetaTwo, phiOne, phiTwo;
 
     // Pneumatics
-    // public final Compressor compressor = new Compressor();
-    // public final DoubleSolenoid leftShift = new DoubleSolenoid(0, 1);
-    // public final DoubleSolenoid rightShift = new DoubleSolenoid(2, 3);
+    public final Compressor compressor = new Compressor();
+    public final DoubleSolenoid leftShift = new DoubleSolenoid(0, 1);
+    public final DoubleSolenoid rightShift = new DoubleSolenoid(2, 3);
 
     // Encoders
     public final Encoder encoderTheta, encoderPhi;
