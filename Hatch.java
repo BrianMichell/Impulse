201aadbb9f7cb182745bce01294ef1cd84130c0a
BrianMichell <ecwhodie@yahoo.com>
2019-02-18 13:59:30 -0600

    Implemented hatch mechanism actuation.

--- Hatch.java
+++ Hatch.java
@@ -1,17 +1,38 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+
 public class Hatch extends Subsystem {
 
+    private DoubleSolenoid hatchClamp;
+    private boolean open;
+    
     public Hatch(Hardware hw){
         super(hw, "Hatch");
+        this.hatchClamp = hw.hatchClamp;
+        this.open = true;
     }
 
     @Override
     protected void actions(){
+        actuate();
     }
 
     @Override
     protected void haltSystem(){
+    }
+
+    private void actuate(){
+        if(open){
+            this.hatchClamp.set(Value.kReverse);
+        } else {
+            this.hatchClamp.set(Value.kForward);
+        }
+    }
+
+    public void setOpen(boolean value){
+        open = !value;
     }
 
 }
